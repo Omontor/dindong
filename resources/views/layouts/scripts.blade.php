dc3ac01f24e684d5df5156a87f772cba5e0a433f
TamaraPimentel <tamyx.p@hotmail.com>
2021-08-26 18:56:19 -0500

    home template usuario normal

---
+++ resources/views/layouts/scripts.blade.php
@@ -0,0 +1,12 @@
+
+        <!-- jQuery  -->
+        <script src="/template/assets/js/jquery.min.js"></script>
+        <script src="/template/assets/js/bootstrap.bundle.min.js"></script>
+        <script src="/template/assets/js/metismenu.min.js"></script>
+        <script src="/template/assets/js/waves.js"></script>
+        <script src="/template/assets/js/feather.min.js"></script>
+        <script src="/template/assets/js/jquery.slimscroll.min.js"></script>
+        <script src="/template/assets/js/jquery-ui.min.js"></script>
+
+        <!-- App js -->
+        <script src="../assets/js/app.js"></script>
