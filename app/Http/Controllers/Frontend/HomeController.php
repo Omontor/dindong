237f31bb67ee981b35ac8953d94ea034da2923fa
Oliver Montor <omontor@gmail.com>
2021-08-30 21:19:23 -0500

    api oficial facturama
    **falta solucionar comprobantes

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -8,83 +8,58 @@ use Redirect;
 use GuzzleHttp\Psr7;
 use GuzzleHttp\Client;
 use Carbon\Carbon;
-
+use Facturama;
 
 class HomeController
 {
     public function index()
     {
-    // Instancia Laravel Facturama
-$api = new \Strappberry\FacturamaLaravel\Classes\ApiFacturama();
-// Obtener instancia de api web
-$apiWeb = $api->web();
+ 
 
+$facturama = new Facturama\Client('pruebas', 'pruebas2011');
 
-// Ejemplo tomado de la documenctación de la api de facturama
-$datosAFacturar = [
-  "Serie" => "R",
-  "Currency" => "MXN",
-  "ExpeditionPlace" => "78116",
-  "PaymentConditions" => "CREDITO A SIETE DIAS",
-  "Folio" => "100",
-  "CfdiType" => "I",
-  "PaymentForm" => "03",
-  "PaymentMethod" => "PUE",
-  "Receiver" => [
-    "Rfc" => "XAXX010101000",
-    "Name" => "Público en general",
-    "CfdiUse" => "P01",
-  ],
-  "Items" => [
-    [
-      "ProductCode" => "10101504",
-      "IdentificationNumber" => "EDL",
-      "Description" => "Estudios de viabilidad",
-      "Unit" => "NO APLICA",
-      "UnitCode" => "MTS",
-      "UnitPrice" => 50.0,
-      "Quantity" => 2.0,
-      "Subtotal" => 100.0,
-      "Taxes" => [
-        [
-          "Total" => 16.0,
-          "Name" => "IVA",
-          "Base" => 100.0,
-          "Rate" => 0.16,
-          "IsRetention" => false,
+$params = [
+    'ExpeditionPlace' => '12345',
+    //'serie' => '',
+    'Folio' => '100',
+    'Currency' => 'MXN',
+    'PaymentConditions' => 'CREDITO A SIETE DIAS',
+    'CfdiType' => 'I',
+    'PaymentForm' => '03',
+    'PaymentMethod' => 'PUE',
+    'Receiver' => [
+           'Rfc' => 'XAXX010101000',
+           'Name' => 'RADIAL SOFTWARE SOLUTIONS',
+           'CfdiUse' => 'P01',
+         ],
+    'Items' => [
+       [
+            'ProductCode' => '10101504',
+            'IdentificationNumber' => 'EDL',
+            'Description' => 'Estudios de viabilidad',
+            'Unit' => 'NO APLICA',
+            'UnitCode' => 'MTS',
+            'UnitPrice' => 50.0,
+            'Quantity' => 2.0,
+            'Subtotal' => 100.0,
+
+            'Taxes' => [
+               [
+                   'Total' => 16.0,
+                   'Name' => 'IVA',
+                   'Base' => 100.0,
+                   'Rate' => 0.16,
+                   'IsRetention' => false,
+               ],
+            ],
+            'Total' => 116.0,
         ],
-      ],
-      "Total" => 116.0,
     ],
-    [
-      "ProductCode" => "10101504",
-      "IdentificationNumber" => "001",
-      "Description" => "SERVICIO DE COLOCACION",
-      "Unit" => "NO APLICA",
-      "UnitCode" => "E49",
-      "UnitPrice" => 100.0,
-      "Quantity" => 15.0,
-      "Subtotal" => 1500.0,
-      "Discount" => 0.0,
-      "Taxes" => [
-        [
-          "Total" => 240.0,
-          "Name" => "IVA",
-          "Base" => 1500.0,
-          "Rate" => 0.16,
-          "IsRetention" => false,
-        ],
-      ],
-      "Total" => 1740.0,
-    ],
-  ],
 ];
 
-// Solicitamos la generación de la factura y el timbrado
-$resultado = $apiWeb->emitirCfdi($datosAFacturar);
-dd($resultado);
+$result = $facturama->post('2/cfdis', $params);
 
-
+printf('<pre>%s<pre>', var_export($result, true));
         /*
         if(Auth::user()->userinfo->count() == 0){
             return redirect()->route('frontend.perfil.index'); 
